5cf2fb3010d7302862c61b1de5bd26517bffb89e
Ritik Kumar <57482671+itsrituu@users.noreply.github.com>
2022-12-25 19:15:32 +0500

    Update Guess count game.py

--- Guess count game.py
+++ Guess count game.py
@@ -1,7 +1,16 @@
+import random
+import time
+print("Welcome to guesing game.Please guess a number between 1 and 100")
+time.sleep(3)
+print("Picking a number...")
+time.sleep(2)
 guess = int(input("what is your guess?:"))
-correct_number = 5
+correct_number = random.randint(1,100)
 guess_cout = 0
 while guess != correct_number:
   guess_cout += 1
-  guess = int(input("what is your guess?:"))
-print(f"The right answer was {correct_number}.It took you right {guess_cout} guesss")
+  if guess < correct_number:
+    guess = int(input("Wrong yon need to guess higher, What is you guess:"))
+  else:
+    guess = int(input("Wrong yon need to guess lower, , What is you guess:"))
+print(f"The right answer was {correct_number}.It took you {guess_cout} guesss")
